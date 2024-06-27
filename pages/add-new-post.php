2646c1c56f8c2d6ab5f150f2346a2d31dc465458
flads <flads.me@gmail.com>
2024-06-26 23:57:01 -0300

    feat: add labels and summary input to add new page

--- pages/add-new-post.php
+++ pages/add-new-post.php
@@ -3,10 +3,16 @@
         <h2 class="m-0">Add New Post</h2>
         <form class="w-100">
             <div class="form-group">
+                <label for="title">Title</label>
                 <input id="title" type="text" name="title" class="w-50" placeholder="Enter title here">
             </div>
             <div class="form-group">
-                <textarea placeholder="Use your markdown skills here"></textarea>
+                <label for="summary">Summary</label>
+                <textarea id="summary" class="summary" name="summary" placeholder="Summarize what your post is about" maxlength="255"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="content">Content</label>
+                <textarea id="content" class="content" name="content" placeholder="Use your markdown skills here"></textarea>
             </div>
             <div class="form-group">
                 <button>Publish</button>
