8d570243d2b6b90dddd283830b5b863d53a2cd28
flads <flads.me@gmail.com>
2025-02-14 19:54:41 -0300

    feat: add favicon

--- public/index.php
+++ public/index.php
@@ -43,6 +43,9 @@ $linkedinUser = $env["LINKEDIN_USER"];
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="/resources/main.css">
 
+    <!-- favicon -->
+    <link rel="icon" type="image/png" href="/resources/favicon.png"/>
+
     <script>
         const spaghettiLocalStorage = JSON.parse(localStorage.getItem('spaghetti'));
 
